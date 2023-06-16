ffdf92c9d98474fc8a9fe9c41cfa8643e8e14f7b
DennisCatana <dennis.joel235@outlook.es>
2023-06-16 11:54:48 -0500

    Metodos y variables corregidas

--- src/Ahorros.java
+++ src/Ahorros.java
@@ -1,2 +1,41 @@
+
 public class Ahorros {
+    private double saldo;
+    private boolean debito;
+    private boolean deposito;
+
+    public Ahorros(double saldo) {
+        this.saldo = saldo;
+        this.debito = false;
+        this.deposito = false;
+    }
+
+
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setDebito(boolean debito) {
+        this.debito = debito;
+    }
+
+    public boolean getDebito() {
+        return debito;
+    }
+
+    public void setDeposito(boolean deposito) {
+        this.deposito = deposito;
+    }
+
+    public boolean getDeposito() {
+        return deposito;
+    }
 }
+
+
+
